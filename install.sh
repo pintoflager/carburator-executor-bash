42be30827f4cdea2a037e84a777ebdd57edd5af9
pintoflager <jaakko0606@gmail.com>
2023-01-22 19:58:02 -0500

    take executors into play

--- install.sh
+++ install.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env sh
 
 # Bash needs bash
-err="Missing required program bash. Please install it before proceeding."
-
-# If remotes don't have carburator installed we can't use it's methods.
-if ! which caburator > /dev/null 2>&1; then
-  if ! which bash > /dev/null 2>&1; then
-    echo "$err" && exit 120
-  fi
-  
-  exit 100
-fi
-
 if ! carburator fn integration-installed bash; then
-  carburator print terminal error "$err" && exit 120
+  carburator print terminal error \
+    "Missing required program bash. Please install it before proceeding."
+  exit 110
 fi
