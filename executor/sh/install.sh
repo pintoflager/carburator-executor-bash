154cb1971e3af6f49026c7aee0fa54b13b521afe
pintoflager <jaakko0606@gmail.com>
2023-02-27 14:42:50 -0500

    Changed my mind

--- executor/sh/install.sh
+++ executor/sh/install.sh
@@ -2,27 +2,24 @@
 
 role="$1"
 
-# App installation tasks on a local client node. Runs first
+# App installation tasks on a client node. Runs first
 if [ "$role" = 'client' ]; then
+    # TODO: get rid of these notifications. Left for debugging only.
     carburator print terminal info "Executing install script on $role"
 fi
 
-# App installation tasks on remote worker node.
-if [ "$role" = 'worker' ]; then
-    carburator print terminal info "Executing install script on $role"
-fi
-
-# App installation tasks on remote commander node.
-if [ "$role" = 'commander' ]; then
+# App installation tasks on remote server node.
+if [ "$role" = 'server' ]; then
+    # TODO: get rid of these notifications. Left for debugging only.
     carburator print terminal info "Executing install script on $role"
 fi
 
 # Bash needs bash
 if ! carburator has program bash; then
-  carburator print terminal error \
-    "Missing required program bash. Please install it before proceeding."
-  exit 120
+    carburator print terminal error \
+        "Missing required program bash. Please install it before proceeding."
+    exit 120
 fi
 
-# Just a reminder, to figure out where (any) script runs:
-# if [[ $(carburator node role) == 'commander' ]]; then ...... ; fi
+# TODO: Just a reminder, to figure out where (any) script runs:
+# if [[ $(carburator node role) == 'client' ]]; then ...... ; fi
